f73c38536dab09d194c7ad597f0829a97b3074e0
syhsyh9696 <sunyuanhao123456@gmail.com>
2018-03-14 12:54:46 +0000

    Add vagrantfiles controller

--- app/controllers/vagrantfiles_controller.rb
+++ app/controllers/vagrantfiles_controller.rb
@@ -1,10 +1,11 @@
 class VagrantfilesController < ApplicationController
   def index
-
+    @vagrantfiles = Vagrantfile.all
   end
 
   def show
-    
+    @vagrantfile = Vagrantfile.find(params[:id])
   end
+
 
 end
