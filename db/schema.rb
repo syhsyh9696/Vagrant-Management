565cbd30f6aca5f115cf25bf3ef696f3ed2bcaa8
syhsyh9696 <sunyuanhao123456@gmail.com>
2018-03-16 09:53:10 +0000

    Add devise view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315130701) do
+ActiveRecord::Schema.define(version: 20180316084516) do
 
   create_table "friendly_id_slugs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "slug", null: false
@@ -264,6 +264,7 @@ ActiveRecord::Schema.define(version: 20180315130701) do
     t.datetime "updated_at", null: false
     t.boolean "admin"
     t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
